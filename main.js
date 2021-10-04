be3dce7443feaf332276880ed0141a436ad0cb04
Sally Chen <sally_chen@brown.edu>
2021-10-04 10:56:30 -0400

    done

--- main.js
+++ main.js
@@ -2,8 +2,29 @@ function addNewList() {
     alert('hello world!');
 }
 
+
 function addListItem() {
-    console.log("hello world!")
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("new-list-item");
+    let newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+    // console.log("hello world!")
 }
 
+function deleteListItem(item) {
+    // remove li element (item) from ol element (item.parentNode)
+     item.parentNode.removeChild(item);
+}
+
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
 
